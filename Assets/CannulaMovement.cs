734dd9ec36976d9c38b27b0ec640c3e976b8f1fa
Khalil <khalil.oumimoun@gmail.com>
2021-01-26 21:09:10 -0600

    Fixed bug

--- Assets/CannulaMovement.cs
+++ Assets/CannulaMovement.cs
@@ -14,7 +14,7 @@ public class CannulaMovement : MonoBehaviour
     private float m_Speed           = 2.0f;              //Sets the speed in which the knot pusher moves.
     private float _nextPosition     = 0.0f;              //Gets the analog value from Arduino.
     List<float> savedAnalogData     = new List<float>(); // List where valid Analog Data being saved. 
-    private int _nextSuggestedValue = 0;                 //This value is used in case ES return "n" to the suggested value. 
+    private int _nextSuggestedValue = 1;                 //This value is used in case ES return "n" to the suggested value. 
     private float _suggestedDataValue;                   //When request rollback is ON, this variable gets sent to ES as a suggested point to rollback to.
     private bool is_FaultInjected   = false;             //Checks if a fault has been detected.
     private float randomFaultNumber;                     //Used to generate a random number to mess with the position. (gets called in FaultInjection)
@@ -23,6 +23,12 @@ public class CannulaMovement : MonoBehaviour
     private SerialPort stream;                           //Used to open the stream communication using portName and baudRate.
     public string portName        = "COM5";              //The port in which Arduino board is connected
     public int baudRate           = 38400;               //baudRate of serial communication
+    string dataString;
+    bool sendRequest = false;
+    bool readyToRead = true;
+    float lastVal = 0;
+    float temp = 0;
+    bool readNegotation = false;
 
 
     /* \\\ Methods:  */
@@ -34,7 +40,7 @@ public class CannulaMovement : MonoBehaviour
         /* \\\ Initializing Serial Communication: \\\ */
         stream             = new SerialPort(portName, baudRate);
         stream.Open();
-        stream.ReadTimeout = 1;
+        stream.ReadTimeout = 1000;
 
         m_Speed            = 2.0f;                     //Set the speed of the GameObject
         InvokeRepeating("saveAnalogPosition", 1f, 1f); //Call the function saveAnalogPosition() every 1 second.
@@ -63,7 +69,11 @@ public class CannulaMovement : MonoBehaviour
     */
     void Update()
     {
-        if (stream.IsOpen)
+        if (Input.GetKeyDown(KeyCode.R)){
+            faultInjectionFunction();
+        }
+
+        if (stream.IsOpen && readyToRead == true)
         {
             try
             {
@@ -75,13 +85,6 @@ public class CannulaMovement : MonoBehaviour
             }
         }
         //Debug.Log(String.Join(", ", savedAnalogData)); //Output all the savedData so far. 
-      
-        if (Input.GetKeyDown(KeyCode.R))               //We simulate a fault injection by pressing the R button.
-        {
-            randomFaultNumber = Random.Range(1, 1000); //Generate a random number from 1 - 1000
-            is_FaultInjected  = true;
-            WriteToArduino("r");                       //Let ES know that there was a fault detected, thus request for Rollback
-        }
     }
 
     /* ReadSerial() -> Reads data from ES. 
@@ -90,9 +93,10 @@ public class CannulaMovement : MonoBehaviour
      */
     void ReadSerial()
     {
-        float lastVal     = _nextPosition;     //Compares last value received with the next value received to check if there is any change.
-        string dataString = stream.ReadLine(); //Reads value as a string from ES
+        dataString = stream.ReadLine(); //Reads value as a string from ES
 
+
+        //Debug.Log("Arduino data: " + dataString);
         switch (dataString)
         {
             case "@":
@@ -101,54 +105,47 @@ public class CannulaMovement : MonoBehaviour
             case "!":
                 Debug.Log("ES finishes Checkpoiting routine");
                 break;
-            case "*":
-                Debug.Log("ES requests Rollback");
-                break;
             case "?":
+                Debug.Log("ES requests Rollback");
+                //readyToRead = false;
+                break;
+            case "*":
                 Debug.Log("ES finished Rollback");
                 break;
-            case "y":                                                                               //ES aggrees about the suggested back up data point
-                recoveryFunction();
+            case "y":
+                readyToRead = false;
+                //recoveryFunction();
+                Vector3 temp2 = knotPusher.transform.position;
+                temp2 = new Vector3(0f, knotPusher.transform.position.y, -15.90f);
+                //temp2 = new Vector3(2f, 0f, 0f);
+                knotPusher.transform.position = temp2;
+                //recoveryFunction();
+                Debug.Log("RECOVERY SUCCESS");
+                readyToRead = true;
+                WriteToArduino("q");                                                                                                     //return the suggestedDataValue in case we need to use it. (if ES aggrees).
                 break;
             case "n":                                                                               //ES disagrees about the suggested back up data point
+                //_nextSuggestedValue++;
+                //_suggestedDataValue = savedAnalogData[savedAnalogData.Count - _nextSuggestedValue]; // Set new data point
+                //string suggestedDataSent = _suggestedDataValue.ToString();                          //Convert new data point to string before sending to ES.
+                //Debug.Log("suggestedDataSent = " + suggestedDataSent);
+                //WriteToArduino(suggestedDataSent);                                                  //New data point sent to ES
                 _nextSuggestedValue++;
-                _suggestedDataValue = savedAnalogData[savedAnalogData.Count - _nextSuggestedValue]; // Set new data point
-                string suggestedDataSent = _suggestedDataValue.ToString();                          //Convert new data point to string before sending to ES.
-                WriteToArduino(suggestedDataSent);                                                  //New data point sent to ES
                 break;
         }
-        
-        float.TryParse(dataString, out _nextPosition);                                       //Converts data received to float. 
-
-        if (lastVal != _nextPosition)                                                        //Compares last Value with the next Value to see if there was a change in value.
-        {
-            if (lastVal - 2 < _nextPosition)
-            {
-                knotPusher.transform.position += transform.right * Time.deltaTime * m_Speed; //Move knotpusher to the right
-            }
-            if (lastVal + 2 > _nextPosition)
-            {
-                knotPusher.transform.position += -transform.right * Time.deltaTime * m_Speed;//Move knotpusher to the left.
-            }
-            if(is_FaultInjected == true)                                                     //Simulate fault injection.
-            {
-                _nextPosition = randomFaultNumber;                                           //Set our next position to the random number generated
-                requestRollback();                                                           // request rollback from ES to correct the error.
-            }
-        }
+        movementFunction(dataString);
     }
 
     /* requestRollback() -> Requests rollback from ES by sending a suggested data point.
      *                      Let ES know that Visualizer wants to Rollback.
      *                      
      */
-    private float requestRollback()
+    private void requestRollback()
     {
+        //readyToRead = true;
         WriteToArduino("r");                                                   //Send 'r' to ES
-        _suggestedDataValue      = savedAnalogData[savedAnalogData.Count - 1]; //Sets the suggested backup point to the last valid saved point.
-        string suggestedDataSent = _suggestedDataValue.ToString();             //Converts the suggested point to string before sending to ES.
-        WriteToArduino(suggestedDataSent);                                     //Send suggested point to ES as string
-        return _suggestedDataValue;                                            //return the suggestedDataValue in case we need to use it. (if ES aggrees).
+        //sendRequest = false;
+        negotiate();
     }
 
     /* recoveryFunction() -> Assuming we get a 'y' from ES. this function does the recovery of the position.
@@ -157,8 +154,14 @@ public class CannulaMovement : MonoBehaviour
     */
     void recoveryFunction()
     {
-        _nextPosition = _suggestedDataValue;            //sets position to the backedup value.
-        WriteToArduino("q");                            //Sends 'q' to ES to declare recovery ended.
+        dataString = _suggestedDataValue.ToString();            //sets position to the backedup value.
+        //movementFunction(dataString);
+
+        //Sends 'q' to ES to declare recovery ended.
+        readyToRead = true;
+        lastVal = 0;
+        
+        //return dataString;
     }
 
     /*WriteToArduino(string) -> Handles the sending data from Unity to ES.
@@ -170,4 +173,50 @@ public class CannulaMovement : MonoBehaviour
         stream.BaseStream.Flush();
     }
 
+    void movementFunction(string dataString)
+    {
+        lastVal = _nextPosition;                                                       //Compares last value received with the next value received to check if there is any change.
+        float.TryParse(dataString, out _nextPosition);                                       //Converts data received to float. 
+
+        if(is_FaultInjected == true)
+        {
+            knotPusher.transform.position += 20 * transform.right * Time.deltaTime * m_Speed; //Move knotpusher to the right
+            is_FaultInjected = false;
+        }
+
+        if (lastVal != _nextPosition)                                                        //Compares last Value with the next Value to see if there was a change in value.
+        {
+            if (lastVal - 2 < _nextPosition)
+            {
+                knotPusher.transform.position += transform.right * Time.deltaTime * m_Speed; //Move knotpusher to the right
+            }
+            if (lastVal + 2 > _nextPosition)
+            {
+                knotPusher.transform.position += -transform.right * Time.deltaTime * m_Speed;//Move knotpusher to the left.
+            }
+        }
+    }
+
+    private void faultInjectionFunction()
+    {
+        is_FaultInjected = true;
+        readyToRead = false;
+        randomFaultNumber = Random.Range(1, 1000); //Generate a random number from 1 - 1000
+        //_nextPosition = randomFaultNumber;                                           //Set our next position to the random number generated
+        movementFunction(randomFaultNumber.ToString());
+        requestRollback();                                                           // request rollback from ES to correct the error.
+    }
+
+    private float negotiate()
+    {
+        _suggestedDataValue = savedAnalogData[savedAnalogData.Count - _nextSuggestedValue]; //Sets the suggested backup point to the last valid saved point.
+        //string suggestedDataSent = _suggestedDataValue.ToString();             //Converts the suggested point to string before sending to ES.
+        WriteToArduino(_suggestedDataValue.ToString());                                     //Send suggested point to ES as string
+        //string negotiationListener = stream.ReadLine();                        //Reads value as a string from ES
+        readNegotation = true;
+        readyToRead = true;
+        ReadSerial();
+        return _suggestedDataValue;                                         //return the suggestedDataValue in case we need to use it. (if ES aggrees).
+       
+    }
 }
